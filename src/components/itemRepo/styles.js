7c0188ad02d373c950d6bc48bc4c65686dbae2ca
Augusto Luba Birsnek <augustobirsnek@gmail.com>
2022-11-07 23:28:43 -0300

    Criando projeto

---
+++ src/components/itemRepo/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+        margin-bottom: 10px;
+    }
+
+    p {
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 10px;
+    }
+
+    .remover {
+        color: #FF0000;
+        cursor: pointer;
+        margin-bottom: 10px;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
